a3aaecf1454fcff1b2023837bcd7c24adbf172d4
PRAKASHOMVAR <omvaradhan@gmail.com>
2026-03-22 12:04:07 +0530

    fix cors

--- edureach-platform/server/src/app.ts
+++ edureach-platform/server/src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:"*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
